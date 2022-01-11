323238cba786f3ecd5fecf584f47f94c10ee5e95
suvarchala12 <bhumasuvarchala55@gmail.com>
2022-01-11 12:57:59 +0530

    uc4

---
+++ RegexProblem/Mobilename.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexPatternMatching
+{
+    public class MobileNumberRegex
+    {
+        public static string MOB_NUM_REGEX = "^[0-9]{1,3}\\s{1}[6789]{1}[0-9]{9}$";
+
+        public bool ValidateMobileNo(string MobileNo)
+        {
+            return Regex.IsMatch(MobileNo, MOB_NUM_REGEX);
+        }
+    }
+}
--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -5,9 +5,13 @@ namespace RegexPatternMatching
     {
         static void Main(string[] args)
         {
-            //UC1
-            FirstNameRegex pattern = new FirstNameRegex();
-            Console.WriteLine(pattern.validateFirstName("Svc"));
+            
+            //UC4
+            MobileNumberRegex pattern4 = new MobileNumberRegex();
+            Console.WriteLine(pattern4.ValidateMobileNo("91 7981267755"));
+
         }
+    }
+}
     }
 }
