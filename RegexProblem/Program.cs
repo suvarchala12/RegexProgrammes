6be26aa8223edb5e401790dbc1b80d3187092d9f
suvarchala12 <bhumasuvarchala55@gmail.com>
2022-01-11 13:04:14 +0530

    uc4

--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -6,8 +6,26 @@ namespace RegexPatternMatching
         static void Main(string[] args)
         {
             //UC1
-            FirstNameRegex pattern = new FirstNameRegex();
-            Console.WriteLine(pattern.validateFirstName("Svc"));
+            //FirstNameRegex pattern = new FirstNameRegex();
+            //Console.WriteLine(pattern.validateFirstName("Svc"));
+
+            //UC2
+            //LastNameRegex pattern2 = new LastNameRegex();
+            //Console.WriteLine(pattern2.validateLastName("Bhuma"));
+
+            //UC3
+            //EmailRegex pattern3 = new EmailRegex();
+            //Console.WriteLine(pattern3.validateEmail("abc.xyz@bl.co.in"));
+
+            //UC4
+            //MobileNumberRegex pattern4 = new MobileNumberRegex();
+            //Console.WriteLine(pattern4.ValidateMobileNo("91 7981267755"));
+
+            //UC5
+            PasswordRegexRule1 pattern5 = new PasswordRegexRule1();
+            Console.WriteLine(pattern5.ValidatePasswordRule1("suvarchalabhuma"));
+
+
         }
     }
 }
