c9f49e1cab5c4dc3685ae14f9701702533b3a243
suvarchala12 <bhumasuvarchala55@gmail.com>
2022-01-11 12:38:03 +0530

    uc2LastNameRegex

--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -6,8 +6,13 @@ namespace RegexPatternMatching
         static void Main(string[] args)
         {
             //UC1
-            FirstNameRegex pattern = new FirstNameRegex();
-            Console.WriteLine(pattern.validateFirstName("Svc"));
+            //FirstNameRegex pattern = new FirstNameRegex();
+            //Console.WriteLine(pattern.validateFirstName("Svc"));
+
+            //UC2
+            LastNameRegex pattern2 = new LastNameRegex();
+            Console.WriteLine(pattern2.validateLastName("Bhuma"));
+
         }
     }
 }
